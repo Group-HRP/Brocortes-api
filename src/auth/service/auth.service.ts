65417d70ab6738cb7f9819147646893988faa86c
Pedroca011 <03ramos011@gmail.com>
2025-05-26 12:08:36 -0300

    fix: retornando o payload no login

--- src/auth/service/auth.service.ts
+++ src/auth/service/auth.service.ts
@@ -72,6 +72,7 @@ export class AuthService {
     const payload = { sub: user.id, email: user.email, role: user.role };
     return {
       access_token: this.jwtService.sign(payload),
+      payload
     };
   }
 }
