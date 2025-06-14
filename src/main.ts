dbc5053c87d7a842c40844d1eb807e9ed231f282
Pedroca011 <03ramos011@gmail.com>
2025-06-14 19:30:17 -0300

    format: formatando o projeto

--- src/main.ts
+++ src/main.ts
@@ -6,11 +6,15 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: ['http://localhost:8081', 'exp://192.168.15.9:8081', 'exp://192.168.0.116:8081'],
+    origin: [
+      'http://localhost:8081',
+      'exp://192.168.15.9:8081',
+      'exp://192.168.0.116:8081',
+    ],
     methods: ['GET', 'POST', 'PATCH', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
-  })
+  });
 
   await app.listen(process.env.PORT ?? 3000);
   console.log(`Rodando na porta ${process.env.PORT}`);
